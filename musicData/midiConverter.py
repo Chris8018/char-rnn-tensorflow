ae39dc5bf371a37afbdcc00e08a5821a29c1f672
Chris <tranuytrieuvi@gmail.com>
2019-04-10 22:20:35 +0100

    Finish midiconverted v1

--- musicData/midiConverter.py
+++ musicData/midiConverter.py
@@ -1,3 +1,18 @@
 import os, os.path
 
 #
+inputPath = 'midi'
+outputPath = 'txt'
+
+inputExtension = '.mid'
+outputExtension = '.txt'
+
+command = 'midicsv'
+
+files = list(map(lambda s: s.split('.')[0], os.listdir(inputPath)))
+#print(files)
+
+# os.system(command)
+for f in files:
+    os.system('{0} {2}/{1}{3} {4}/{1}{5}'.format(command, f, inputPath, inputExtension, outputPath, outputExtension))
+    #print('{0} {2}/{1}{3} {4}/{1}{5}'.format(command, f, inputPath, inputExtension, outputPath, outputExtension))
